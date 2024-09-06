94ac492d40d395aa76b281ecddc66c78dca1282f
Mohamed Refaat <Mohamed Refaat@DESKTOP-G94OA3M>
2024-09-07 02:28:02 +0300

    Add project files.

---
+++ Demo/NewFolder1/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.NewFolder1
+{
+    public class MoviesController :Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "All Movies";
+        }
+        //[Authorize] // هل فيه Token ?
+        [HttpGet]
+        //[AcceptVerbs("Get")]
+        public string GetMovie(int id) {
+
+
+            return $"Movie with Id : {id}";
+        }
+
+
+        //[ActionName("ConfirmGetMovie")]
+        //public string GetMovie(String Name)
+        //{
+
+
+        //    return $"Movie with Id : {Name}";
+        //}
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,35 @@
+namespace Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region configure
+
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+            app.UseRouting();
+            app.MapGet("/", () => "Hello World!");
+
+            app.MapGet("/XX{id:int}", async context =>
+            {
+
+
+                await context.Response.WriteAsync($"Id = {context.Request.RouteValues["id"]}");
+
+
+
+            });
+            app.MapControllerRoute(
+                name: "default",
+                pattern:"{controller}/{action}/{id?}" );
+
+
+            app.Run();
+
+            #endregion
+
+        }
+    }
+}
