751a4238a6579df042eab76d9512e5f2a7805be9
mohamed refaat <mr9820862@gmail.com>
2024-09-07 04:11:59 +0300

    Action ReturnTypes

--- Demo/NewFolder1/MoviesController.cs
+++ Demo/NewFolder1/MoviesController.cs
@@ -10,14 +10,17 @@ namespace Demo.NewFolder1
         {
             return "All Movies";
         }
+
+
+
         //[Authorize] // هل فيه Token ?
-        [HttpGet]
+        //[HttpGet]
         //[AcceptVerbs("Get")]
-        public string GetMovie(int id) {
+        //public string GetMovie(int id) {
 
 
-            return $"Movie with Id : {id}";
-        }
+        //    return $"Movie with Id : {id}";
+        //}
 
 
         //[ActionName("ConfirmGetMovie")]
@@ -27,5 +30,36 @@ namespace Demo.NewFolder1
 
         //    return $"Movie with Id : {Name}";
         //}
+
+        [HttpGet] // GET: baseUrl/Movies/GetMovie/{id}
+        public IActionResult GetMovie(int id)
+        {
+            if (id == 0)
+            {
+                // return new BadRequestResult();
+                return BadRequest();
+            }
+
+            if (id == 100)
+            {
+                // return new NotFoundResult();
+                return NotFound();
+            }
+
+            // ContentResult result = new ContentResult();
+            return Content($"<h1> Movie with Id: {id} </h1>", "text/html");
+        }
+
+
+
+        public IActionResult Hamda()
+        {
+            // RedirectResult result = new RedirectResult("https://wuzzuf.net/jobs/egypt");
+
+
+            RedirectToActionResult result = new RedirectToActionResult("GetMovie", "Movies", new { id = 10 });
+
+            return result;
+        }
     }
 }
